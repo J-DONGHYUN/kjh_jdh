a3d69da890d05264644e66f69c7b089c2b8a2ab1
wlsh44 <k87913j@gmail.com>
2026-07-02 16:31:46 +0900

    style: 필요없는 주석 제거

--- src/main/java/project/kjhjdh/ibid/auth/presentation/resolver/LoginUser.java
+++ src/main/java/project/kjhjdh/ibid/auth/presentation/resolver/LoginUser.java
@@ -5,10 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * 인증된 사용자 정보({@link project.kjhjdh.ibid.auth.domain.UserInfo})를
- * 컨트롤러 파라미터로 주입받기 위한 애노테이션.
- */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoginUser {
