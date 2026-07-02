225758178af100b1705cb8407e46b5e1c79d9f92
wlsh44 <k87913j@gmail.com>
2026-07-02 16:38:50 +0900

    style: 필요없는 주석 제거

--- src/main/java/project/kjhjdh/ibid/user/domain/Email.java
+++ src/main/java/project/kjhjdh/ibid/user/domain/Email.java
@@ -7,9 +7,6 @@ import jakarta.persistence.Embeddable;
 import project.kjhjdh.ibid.common.exception.BusinessException;
 import project.kjhjdh.ibid.common.exception.ErrorCode;
 
-/**
- * 이메일 값 객체. 생성 시점에 형식을 검증하므로 유효하지 않은 이메일로는 인스턴스가 만들어지지 않는다.
- */
 @Embeddable
 public record Email(
         @Column(name = "email", nullable = false, unique = true)
